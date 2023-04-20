7c8dccb4bc2cd5f2ea31d07a9ec6a7396ff69026
Oleg Barbashow <teosofio@gmail.com>
2023-04-20 21:43:16 +0300

    Created component wireframes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import App from "./components/app/app";
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 );
